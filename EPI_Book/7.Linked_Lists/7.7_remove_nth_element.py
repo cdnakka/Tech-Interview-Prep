dbc009ac6c709f8bd01fddc95f718ee6f25f3fac
Chanakya Dev <chanakyadev@iitj.ac.in>
2022-09-29 15:19:04 -0400

    adding remove nth element

---
+++ EPI_Book/7.Linked_Lists/7.7_remove_nth_element.py
@@ -0,0 +1,18 @@
+'''
+from LC
+
+'''
+
+class Solution:
+    def removeNthFromEnd(self, head, n):
+        fast = slow = head
+        for _ in range(n):
+            fast = fast.next
+        #edge case
+        if not fast:
+            return head.next
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return head
